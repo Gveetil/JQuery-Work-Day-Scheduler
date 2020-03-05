c3ef19c433fb12367677643e14518863cde49650
Geethanjali Veetil <gtveetil@gmail.com>
2020-03-05 14:15:16 +1100

    Updated code to select date

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
 
     var currentDayDisplayEl = $("#currentDay");
     var timeblockContainerEl = $("#timeblockContainer");
+    var previousEl = $("#previous");
+    var nextEl = $("#next");
     var startBusinessHours = 9;
     var endBusinessHours = 17;
     // Selected date is today
@@ -14,7 +16,9 @@ $(document).ready(function () {
      */
     function renderScheduler() {
         currentDayDisplayEl.text(selectedDate.format(schedulerDayDisplayFormat));
+        timeblockContainerEl.text("");
         var timeSelection = moment(selectedDate);
+        var currentTime = moment();
         timeSelection.set("hour", startBusinessHours);
         while (timeSelection.hours() <= endBusinessHours) {
             var task = taskHelper.getTask(timeSelection);
@@ -22,30 +26,48 @@ $(document).ready(function () {
             if (task != null) {
                 description = task.description;
             }
-            appendTimeBlock(timeSelection.format('ha'), description);
-
+            appendTimeBlock(timeSelection, description, currentTime);
             timeSelection.add(1, 'hours');
         }
     }
 
-    function appendTimeBlock(time, description) {
+    function appendTimeBlock(timeSelection, description, currentTime) {
         var row = $("<div class='row time-block no-gutters'>");
-        var column1 = $("<div class='col-sm-1 hour'>");
-        column1.text(time);
-        row.append(column1);
-        var column2 = $("<div class='col-sm-10 present description'>");
+        var columnHour = $("<div class='col-2 col-md-1 hour'>");
+        columnHour.text(timeSelection.format('ha'));
+        row.append(columnHour);
+        var columnDescription = $("<div class='col-8 col-md-10 description'>");
         var textTask = $("<textarea class='w-100 h-100'>");
         textTask.val(description);
-        column2.append(textTask);
-        row.append(column2);
-        var column3 = $("<div class='col-sm-1 saveBtn'>");
-        column3.append($("<i class='fas fa-save'>"));
-        row.append(column3);
+        columnDescription.append(textTask);
+        if (timeSelection.isSame(currentTime, 'hour'))
+            columnDescription.addClass("present");
+        else if (currentTime.isAfter(timeSelection)) {
+            columnDescription.addClass("past");
+        }
+        else
+            columnDescription.addClass("future");
+        row.append(columnDescription);
+        var columnSave = $("<div class='col-2 col-md-1 saveBtn'>");
+        columnSave.append($("<i class='fas fa-save'>"));
+        row.append(columnSave);
         timeblockContainerEl.append(row);
     }
 
     renderScheduler();
     timeblockContainerEl.on("click", timeblockContainerClicked)
+    previousEl.on("click", previousButtonCliicked);
+    nextEl.on("click", nextButtonCliicked);
+
+    function previousButtonCliicked() {
+        selectedDate.subtract(1, 'days');
+        renderScheduler();
+    }
+
+    function nextButtonCliicked() {
+        selectedDate.add('days', 1);
+        renderScheduler();
+    }
 
     function timeblockContainerClicked() {
         // Check if Save icon was clicked
@@ -62,7 +84,7 @@ $(document).ready(function () {
         taskDateTime.set({
             'year': selectedDate.year(),
             'month': selectedDate.month(),
-            'day': selectedDate.day()
+            'date': selectedDate.date()
         })
         if (taskDescription == "") {
             //No description found -- remove task, if it exists
